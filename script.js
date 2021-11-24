d3d496651136027ccd66ae3a2a0737dc1a87997b
Adebayo Olamilekan <75235148+OleanjiKingCode@users.noreply.github.com>
2021-11-24 15:48:08 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+
+$(document).ready(function(){
+
+$("#banner-slider .owl-carousel").owlCarousel({
+        items:1,
+        loop:true,
+        autoplay:true,
+        autoplayHoverPause:true,
+        dots:true
+
+
+    });
+
+
+});
